ee70ac8d5f3dbf4472e152758cac810c0e279ed1
Paulo Alves <front@agencianovi.com.br>
2019-07-21 15:45:17 -0300

    produtos

---
+++ produtos.php
@@ -0,0 +1,113 @@
+<?php include 'components/header.php'; ?>
+
+	<section class="modelos interna">
+		<div class="container">
+			<h1>PRODUTOS</h1>
+			<div class="row">
+				<div class="col-md-3 col-sm-3">
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="heading0">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse0" aria-expanded="false" aria-controls="collapse0">TRAILER HAMBURGUER</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                                <div class="panel-body">
+                                    <ul>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    </ul>
+                                </div><!-- panel-body -->
+                            </div><!-- collapseOne -->
+                            <div class="panel-heading" role="tab" id="heading1">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse1" aria-expanded="false" aria-controls="collapse1">TRAILER HAMBURGUER</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
+                                <div class="panel-body">
+                                    <ul>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    	<li><a href="">TRAILER HAMBURGUER</a></li>
+                                    </ul>
+                                </div><!-- panel-body -->
+                            </div><!-- collapseOne -->
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a href="">TRAILER HAMBURGUER</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a href="">TRAILER HAMBURGUER</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a href="">TRAILER HAMBURGUER</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a href="">TRAILER HAMBURGUER</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                        </div><!-- panel -->
+                    </div><!-- panel-group -->
+				</div><!-- col-md-3 col-sm-3 -->
+				<div class="col-md-9 col-sm-9">
+					<div class="row">
+						<div class="col-md-4 col-sm-4">
+							<div class="item">
+								<a href=""><figure><img src="assets/images/vitrine.jpg" alt=""></figure></a>
+								<a class="btn-mais">+</a>
+								<a href=""><h4>TRAILER PARA HAMBURGUER</h4></a>
+							</div><!-- item -->
+						</div><!-- col-md-4 col-sm-4 -->
+						<div class="col-md-4 col-sm-4">
+							<div class="item">
+								<a href=""><figure><img src="assets/images/vitrine.jpg" alt=""></figure></a>
+								<a class="btn-mais">+</a>
+								<a href=""><h4>TRAILER PARA HAMBURGUER</h4></a>
+							</div><!-- item -->
+						</div><!-- col-md-4 col-sm-4 -->
+						<div class="col-md-4 col-sm-4">
+							<div class="item">
+								<a href=""><figure><img src="assets/images/vitrine.jpg" alt=""></figure></a>
+								<a class="btn-mais">+</a>
+								<a href=""><h4>TRAILER PARA HAMBURGUER</h4></a>
+							</div><!-- item -->
+						</div><!-- col-md-4 col-sm-4 -->
+						<div class="col-md-4 col-sm-4">
+							<div class="item">
+								<a href=""><figure><img src="assets/images/vitrine.jpg" alt=""></figure></a>
+								<a class="btn-mais">+</a>
+								<a href=""><h4>TRAILER PARA HAMBURGUER</h4></a>
+							</div><!-- item -->
+						</div><!-- col-md-4 col-sm-4 -->
+						<div class="col-md-4 col-sm-4">
+							<div class="item">
+								<a href=""><figure><img src="assets/images/vitrine.jpg" alt=""></figure></a>
+								<a class="btn-mais">+</a>
+								<a href=""><h4>TRAILER PARA HAMBURGUER</h4></a>
+							</div><!-- item -->
+						</div><!-- col-md-4 col-sm-4 -->
+						<div class="col-md-4 col-sm-4">
+							<div class="item">
+								<a href=""><figure><img src="assets/images/vitrine.jpg" alt=""></figure></a>
+								<a class="btn-mais">+</a>
+								<a href=""><h4>TRAILER PARA HAMBURGUER</h4></a>
+							</div><!-- item -->
+						</div><!-- col-md-4 col-sm-4 -->
+					</div><!-- row -->
+				</div><!-- col-md-9 col-sm-9 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- modelos -->
+	
+<?php include 'components/footer.php'; ?>
